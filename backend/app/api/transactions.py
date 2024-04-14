a26589f1931a766cefbbed182f11cf83bad9df22
avliserdna <andre.christian.silva@gmail.com>
2024-04-13 21:53:51 -0500

    Finished transactions get route.

--- backend/app/api/transactions.py
+++ backend/app/api/transactions.py
@@ -2,3 +2,10 @@ from flask import Blueprint, jsonify, session, request
 from flask_login import login_required, current_user
 from app.models import Transaction, db
 from app.forms import TransactionForm
+
+transaction_routes = Blueprint('transactions', __name__)
+
+@transaction_routes.route('/')
+def transactions():
+  transactions = Transaction.query.all()
+  return {transaction.id: transactions.to_dict() for transaction in transactions}
